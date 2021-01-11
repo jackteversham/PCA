1a9b8b99f9a0b86785477af1cf4492d06fc2d1eb
Jack Teversham <jacktevers@gmail.com>
2021-01-11 10:46:12 +0200

    Minor changes.

--- pca.cpp
+++ pca.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 int main(){
-
     Eigen::MatrixXd avgRainfall(64,2); //64 2 variable data points
 
     std::ifstream inputFile("../Data/Data.txt");
@@ -15,39 +14,60 @@ int main(){
         string line2 = "";
 
         int count1 = 0;
-        int count2 = 0;
-
+    
         inputFile >> line;
         inputFile >> line; //discard the first two lines
 
         while(!inputFile.eof()){
-            // getline(inputFile, line, '\n');
-            // getline(inputFile, line, '\n');
             inputFile >> location;
             inputFile >> line; 
             inputFile >> line2;
-    
-            // cout<< line<<endl;
-            // cout << line2<<endl;
 
             avgRainfall(count1, 0) = stod(line);
             avgRainfall(count1, 1) = stod(line2);
-
             count1 ++;
-           
-           // avgRainfall(count1, count2) = ;
-
-           // value =stod(line);         
         }
-
     }else{
         std::cout << "File now found, exiting.";
         return 0;
     }
+    inputFile.close();
 
-    cout << avgRainfall<<endl;
+    ofstream outputFile("../output.txt");
 
-    
+    outputFile << "-------------------------------------\n   Question 1: Eigen Values\n-------------------------------------\n";
 
+    Eigen::MatrixXd centered = avgRainfall.rowwise() - avgRainfall.colwise().mean();
+    Eigen::MatrixXd covarianceMatrix = (centered.adjoint()*centered/double(avgRainfall.rows()-1));
+
+    Eigen::EigenSolver<Eigen::MatrixXd> eigenSolver(covarianceMatrix);
+
+      complex<double> eigenValue1 = eigenSolver.eigenvalues()[0];
+      complex<double> eigenValue2 = eigenSolver.eigenvalues()[1];
+
+      outputFile << "PCA1: "<<eigenValue1.real()<<endl;
+      outputFile << "PCA2: "<<eigenValue2.real()<<endl;
+
+    outputFile << "-------------------------------------\n   Question 2: Eigen Vectors\n-------------------------------------\n";
+    outputFile <<"PCA1:\n"<<eigenSolver.eigenvectors().col(0).real() << endl;
+    outputFile <<"PCA2:\n"<<eigenSolver.eigenvectors().col(1).real() << endl;
+
+    outputFile << "-------------------------------------\n   Question 3: Covariance Matrix\n-------------------------------------\n";
+    outputFile << covarianceMatrix << endl;
+
+    complex<double> totalVariance = eigenValue1 + eigenValue2;
+
+    outputFile << "-------------------------------------\n   Question 4: Total Variance\n-------------------------------------\n";
+    outputFile << totalVariance.real() << endl;
+
+    complex<double> propotion1 = eigenValue1/totalVariance *100.0;
+    complex<double> propotion2 = eigenValue2/totalVariance *100.0;
+
+    outputFile << "-------------------------------------\n   Question 4: Proportion of Variance\n-------------------------------------\n";
+    outputFile << "PCA1: "<<propotion1.real() <<"%  "  << endl;
+    outputFile << "PCA2: "<<propotion2.real() <<"%  "  << endl;
+    outputFile.close();
+
+    cout <<"\nResults printed to output.txt in directory above.\n\n";
 return 0;
 }
